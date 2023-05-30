e6529ae28e23d406ad19c1794ac53237333b0ec0
David Palacio <gamerpg08@gmail.com>
2023-05-30 11:47:32 -0500

    first api version

---
+++ src/common/parse-int/parse-int.pipe.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ArgumentMetadata, Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+
+@Injectable()
+export class ParseIntPipe implements PipeTransform {
+  transform(value: string, metadata: ArgumentMetadata) {
+    const val = parseInt(value, 10);
+    if(isNaN(val)) {
+      throw new BadRequestException(`${value} is not an number`)
+    }
+    return val; 
+  }
+}
